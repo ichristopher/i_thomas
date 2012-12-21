c30ffff04d186488454c976ff5e3277be38d6848
Ian Christopher <Ian@LittleBendRoad.com>
2012-12-21 11:47:41 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IThomas::Application.config.secret_token = '9f36abdaed7a164ccaa097c1d78badb257846bcec585610075bc99717b3b9fdfa7349a4025bf8f4cdbf310fb4b51e9fcf371ee4a9e0f8d5da76b0a6079c270c1'
