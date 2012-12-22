07c0e0b0a2bd1afe0af5846c03b7c46690d25e20
Ian Christopher <Ian@LittleBendRoad.com>
2012-12-22 17:28:56 -0600

    Add page title helper, modify gitignore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Helper for returning full title with site name, or site name if not provided by page
+	def full_title(page_title)
+		base_title = "iThomas"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+	
 end
