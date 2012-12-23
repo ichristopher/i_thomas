c32e6a6a44af48bed3cb170669b92f4f93aa82fb
Ian Christopher <Ian@LittleBendRoad.com>
2012-12-23 11:51:55 -0600

    Finish layout and add routes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.9'
+gem 'bootstrap-sass', '2.1' #CSS framework from Twitter
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,37 +7,28 @@ require 'spec_helper'
 # use Command+Shift+T to run all tests in this file
 # use Command+Shift+E to run the "last" test, i.e., the previously run test
 
+# tests for Home page ported to terse style leveraging spec/support/utilities.rb
+
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path } 
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "iThomas")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => "| Home")
-    end
-
+    it { should have_selector('h1',    text: 'iThomas') }
+    it { should have_selector('title', text: full_title('')) }
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "iThomas | Help")
     end
@@ -46,12 +37,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => "iThomas | About Us")
     end
@@ -60,12 +51,12 @@ describe "Static pages" do
 describe "Contact page" do
 
     it "should have the h1 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact Us')
     end
 
     it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                     :text => "iThomas | Contact Us")
     end
